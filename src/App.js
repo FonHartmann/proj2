f26a76f793f044588ba3d3e23d4b7f357a8effb0
FonHartmann <zand3256@gmail.com>
2022-08-19 09:28:05 +0300

    new new proj

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://e7.pngegg.com/pngimages/440/850/png-clipart-pepe-frog-illustration-twitch-pepe-the-frog-youtube-video-game-frog-television-animals.png' className="App-logo" alt="logo" />
+        <p style="mg-2">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
