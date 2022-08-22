b1293eedc354738d9943d07892c27cb225628682
FonHartmann <110547106+FonHartmann@users.noreply.github.com>
2022-08-22 11:56:47 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import * as React from "react";
 import { Button } from "@mui/material";
 import Stack from "@mui/material/Stack";
+import LogoutTwoToneIcon from '@mui/icons-material/LogoutTwoTone';
 
 export const Header = () => {
   return (
@@ -13,8 +14,8 @@ export const Header = () => {
               <Button variant="contained" component={Link} to="/">
                 Home
               </Button>
-              <Button variant="contained" component={Link} to="/login">
-                LogIn
+              <Button variant="contained" endIcon={<LogoutTwoToneIcon />} component={Link} to="/login">
+                LogOut
               </Button>
             </Stack>
           </nav>
