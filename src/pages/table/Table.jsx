5c492fe100ee059f17801e1fbdb820bd50bca81e
FonHartmann <zand3256@gmail.com>
2022-09-02 14:38:58 +0300

    5

--- src/pages/table/Table.jsx
+++ src/pages/table/Table.jsx
@@ -1,22 +1,26 @@
+
+
 export const Table = () => {
   return (
-    <div>
-      <table width="1518" cellSpacing="0" cellPadding="0" border="1">
-        <tbody>
-          <tr>
-            <td colSpan="3" width="50">
-              <p>&nbsp;</p>
-            </td>
-            <td colSpan="24" width="956">
-              <h1>
-               <span>ИОТ №</span>
-                <span>&nbsp;&nbsp;&nbsp;&nbsp; </span>
-                <span></span>
-              </h1>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+<>
+<div className="tab__wrapper">
+    <table width="1518" cellSpacing="0" cellPadding="0" border="1">
+      <tbody>
+        <tr>
+          <td colSpan="24" width="956">
+            <h1>
+              <span>ИОТ №</span>
+              <span>&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </h1>
+          </td>
+          <td colSpan="7" width="235"></td>
+          <td colSpan="24" width="165"></td>
+          <td colSpan="24" width="64"></td>
+          <td colSpan="24" width="48"></td>
+        </tr>
+      </tbody>
+    </table>
+</div>
+  </>
   );
 };
